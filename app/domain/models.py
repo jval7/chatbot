c353d8794a40533d8485ec9a54eb3aff782dbd17
Makhai412 <higueralozanoandres@gmail.com>
2024-10-03 17:16:04 -0500

    Test chatbot realizados

--- app/domain/models.py
+++ app/domain/models.py
@@ -1,28 +1,26 @@
-from typing import cast
-
+from typing import cast, List
 import shortuuid
 from langchain_core.messages import human, ai
 from pydantic import v1 as pd1
 
-
 def generate_uuid() -> str:
     return cast(str, shortuuid.uuid())
 
-
-# Entities
-
-
 class Conversation(pd1.BaseModel):
-    history: list[ai.AIMessage | human.HumanMessage] = pd1.Field(default_factory=list)
+    history: List[ai.AIMessage | human.HumanMessage] = pd1.Field(default_factory=list)
 
+    class Config:
+        arbitrary_types_allowed = True
 
-# Aggregates
 class Chat(pd1.BaseModel):
     id: str = pd1.Field(default_factory=generate_uuid)
     conversation: Conversation = pd1.Field(default_factory=Conversation)
 
-    def update_conversation(self, current_conversation: list[ai.BaseMessage]) -> None:
+    class Config:
+        arbitrary_types_allowed = True
+
+    def update_conversation(self, current_conversation: List[ai.BaseMessage]) -> None:
         self.conversation.history = current_conversation
 
-    def get_conversation_history(self) -> list[ai.BaseMessage]:
+    def get_conversation_history(self) -> List[ai.BaseMessage]:
         return self.conversation.history
